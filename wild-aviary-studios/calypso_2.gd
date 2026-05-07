9a8107c41b2fc14bc9c6347c221312c760113fc1
rhit-mackeycb <127052236+rhit-mackeycb@users.noreply.github.com>
2026-05-07 14:47:43 -0400

    Double jump working

--- wild-aviary-studios/calypso_2.gd
+++ wild-aviary-studios/calypso_2.gd
@@ -4,18 +4,22 @@ extends CharacterBody2D
 const SPEED = 325.0
 const JUMP_VELOCITY = -650.0
 
+var extra_jumps = 0
+var max_extra_jumps = 0
 
 func _physics_process(delta: float) -> void:
-	## Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	else: 
+		extra_jumps = max_extra_jumps
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("ui_accept"):
+		if is_on_floor():
+			velocity.y = JUMP_VELOCITY
+		elif extra_jumps > 0:
+			velocity.y = JUMP_VELOCITY
+			extra_jumps -= 1
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
@@ -23,3 +27,14 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+func _on_area_entered(area: Area2D) -> void:
+	if area.is_in_group("shrine"):
+		max_extra_jumps = 1
+		extra_jumps = 1
+
+
+func _on_pickup_detector_area_entered(area: Area2D) -> void:
+	if area.is_in_group("shrine"):
+		max_extra_jumps = 1
+		extra_jumps = 1
