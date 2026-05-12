cca8c9c92d9c36b005e269a002b7db1199c9d1bb
rhit-mackeycb <127052236+rhit-mackeycb@users.noreply.github.com>
2026-05-12 17:30:26 -0400

    All enemies can be slain

---
+++ wild-aviary-studios/area_2d_boss.gd
@@ -0,0 +1,26 @@
+extends Area2D
+
+@export var health := 10
+var can_take_damage := true
+
+func _ready() -> void:
+	area_entered.connect(_on_area_entered)
+
+func _on_area_entered(area: Area2D) -> void:
+	print("Touched by: ", area.name)
+
+	if not can_take_damage:
+		return
+
+	if area.name == "Left_Spear" or area.name == "Right_Spear":
+		can_take_damage = false
+		health -= 1
+
+		print(name, " health: ", health)
+
+		if health <= 0:
+			queue_free()
+			return
+
+		await get_tree().create_timer(0.5).timeout
+		can_take_damage = true
