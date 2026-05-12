9a91afb7267c2e782d99f2cd5b8667efb94afb48
rhit-mackeycb <127052236+rhit-mackeycb@users.noreply.github.com>
2026-05-12 14:36:44 -0400

    Calypso uses spears

--- wild-aviary-studios/calypso_2.gd
+++ wild-aviary-studios/calypso_2.gd
@@ -1,11 +1,19 @@
 extends CharacterBody2D
 
-
 const SPEED = 325.0
 const JUMP_VELOCITY = -650.0
 
+@onready var left_spear = $Left_Spear
+@onready var right_spear = $Right_Spear
+
 var extra_jumps = 0
 var max_extra_jumps = 0
+
+var spear_on_cooldown := false
+
+func _ready() -> void:
+	left_spear.visible = false
+	right_spear.visible = false
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
@@ -26,13 +34,35 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
+	if Input.is_action_just_pressed("spear_right"):
+		use_spear(left_spear)
+
+	if Input.is_action_just_pressed("spear_left"):
+		use_spear(right_spear)
+
 	move_and_slide()
+
+func use_spear(spear: Node2D) -> void:
+	if spear_on_cooldown:
+		return
+
+	spear_on_cooldown = true
+
+	left_spear.visible = false
+	right_spear.visible = false
+
+	spear.visible = true
+
+	await get_tree().create_timer(0.5).timeout
+	spear.visible = false
+
+	await get_tree().create_timer(1.0).timeout
+	spear_on_cooldown = false
 
 func _on_area_entered(area: Area2D) -> void:
 	if area.is_in_group("shrine"):
 		max_extra_jumps = 1
 		extra_jumps = 1
-
 
 func _on_pickup_detector_area_entered(area: Area2D) -> void:
 	if area.is_in_group("shrine"):
